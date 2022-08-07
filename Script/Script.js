656a767bef2b7c05950fe25e95339f092e466f3e
HankHill <hhc3h8@gmail.com>
2022-08-07 16:10:50 -0500

    got one thing to kinda work; gonna commit before working on it more

--- Script/Script.js
+++ Script/Script.js
@@ -1,6 +1,40 @@
 var APIkey = '600c61e27cf44906cdc33478a6409187';
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" +
- city + "&appid=" + APIKey;
-var rightHere = "";
+//var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+//city + "&units=imperial" + "&appid=" + APIKey;
 var lastSearched = [];
 var today = moment().format('L');
+
+var handleErrors = function (response) {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+};
+
+function currentWeather (city) {
+
+   var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIkey}`;
+    
+    $.ajax ({
+        url: queryURL,
+        method: "GET"
+    }).then(function(cityWeatherResponse){
+        console.log(cityWeatherResponse);
+    });
+
+}
+
+
+$('#searchBtn').on('click', function(event) {
+    event.preventDefault();
+    var city = $('#userSearch').val().trim();
+    currentWeather(city);
+    if (!lastSearched.includes(city)) {
+        lastSearched.push(city);
+        var searched = $('<button class = "btn-danger-lg mb-2">$(city)</button>');
+        $('#cities').append(searched);
+    };
+
+    localStorage.setItem(city, JSON.stringify(lastSearched));
+    console.log(lastSearched);
+});

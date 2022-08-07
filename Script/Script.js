1ac62c749239927fb17c68d27e04abeab1a7df8e
HankHill <hhc3h8@gmail.com>
2022-08-07 15:13:34 -0500

    re-adding the link thing until I figure it out

--- Script/Script.js
+++ Script/Script.js
@@ -1,6 +1,6 @@
 var APIkey = '600c61e27cf44906cdc33478a6409187';
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+ city + "&appid=" + APIKey;
 var rightHere = "";
 var lastSearched = [];
 var today = moment().format('L');
-
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}";

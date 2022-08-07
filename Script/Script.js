ada2831d1671cdea94176c692a8f63f53f6d99e6
HankHill <hhc3h8@gmail.com>
2022-08-07 18:03:12 -0500

    kind of doing stuff; will make it do right stuff soon

--- Script/Script.js
+++ Script/Script.js
@@ -3,6 +3,7 @@ var APIkey = '600c61e27cf44906cdc33478a6409187';
 //city + "&units=imperial" + "&appid=" + APIKey;
 var lastSearched = [];
 var today = moment().format('L');
+let i = 0;
 
 var handleErrors = function (response) {
     if (!response.ok) {
@@ -20,8 +21,15 @@ function currentWeather (city) {
         method: "GET"
     }).then(function(cityWeatherResponse){
         console.log(cityWeatherResponse);
-    });
 
+        var icon = cityWeatherResponse.weather[0].icon;
+        var iconUrl = 'https://openweathermap.org/img/w/${iconCode}.png';
+        // AHHHHHH I HATE THIS BEING ONE LINE I HATE THIS BEING ONE LINE I HATE THIS BEING ONE LINE!
+        var rightHere = $('<h1 id="townDisplay">${cityWeatherResponse.name} ${today} <img src = "${iconUrl}" alt = "{cityWeatherResponse.weather[0].description}" /></h1> <p>Temp: ${cityWeatherResponse.main.temp} °F</p><p>Humidity: ${cityWeatherResponse.main.humidity}%</p><p>Wind speed: ${cityWeatherResponse.main.wind.speed}MPH</p>');
+
+        $('#townDisplay').append(rightHere);
+
+    });
 }
 
 
@@ -32,10 +40,26 @@ $('#searchBtn').on('click', function(event) {
     if (!lastSearched.includes(city)) {
         lastSearched.push(city);
         var searched = $('<button></button>');
-        searched.attr('class', 'btn-danger', 'btn-lg', 'float-left', 'mb-2')
+        searched.text(lastSearched);
+        searched.attr('class', 'btn-danger', 'mb-2');
         $('#cities').append(searched);
     };
 
     localStorage.setItem("city", JSON.stringify(lastSearched));
-    console.log(lastSearched);
+    //console.log(lastSearched);
+});
+
+$(document).on('click', '#cities', function() {
+    var cityList = $(this).text();
+    currentWeather(cityList);
+});
+
+$(document).ready(function() {
+    var searchHistory = JSON.parse(localStorage.getItem("city"));
+
+    if (searchHistory !== null) {
+        var searchIndex = searchHistory.length - 1;
+        var lastCity = searchHistory[searchIndex];
+        currentWeather(lastCity);
+    }
 });

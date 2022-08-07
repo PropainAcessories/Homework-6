5cf5a67f937d21c96e4e09ca0b471215f303b593
HankHill <hhc3h8@gmail.com>
2022-08-07 15:10:11 -0500

    nothing working; new things tried later/tomorrow

--- Script/Script.js
+++ Script/Script.js
@@ -1,28 +1,6 @@
 var APIkey = '600c61e27cf44906cdc33478a6409187';
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" +
- city + "&appid=" + APIKey;
 var rightHere = "";
-var lastSearched = "";
+var lastSearched = [];
+var today = moment().format('L');
 
-function errorHandler() {
-    if (!response.ok) {
-        throw Error(response.statusText);
-    }
-    return response;
-}
-
-function getWeather (event) {
-    var city = $('#userSearch').val();
-    rightHere = $('#userSearch').val();
-
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    
-    fetch(queryURL)
-    .then(errorHandler)
-    .then((response) => {
-        return response.json();
-    })
-    .then((response => {
-        return response.json();
-    }))
-}
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}";
